218e573dbdd20175b3db3f0b789a7b3c9b2f8b03
Jan Chaloupka <jchaloup@redhat.com>
2026-03-30 18:54:33 +0200

    feat(api/secondaryscheduler): new topology field for enabling an HA mode

--- pkg/apis/secondaryscheduler/v1/types.go
+++ pkg/apis/secondaryscheduler/v1/types.go
@@ -2,6 +2,7 @@ package v1
 
 import (
 	operatorv1 "github.com/openshift/api/operator/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -33,6 +34,58 @@ type SecondarySchedulerSpec struct {
 
 	// SchedulerImage sets the container image url to be pulled for the custom scheduler that's deployed
 	SchedulerImage string `json:"schedulerImage"`
+
+	// topology defines scheduling constraints for the secondary scheduler instances
+	// +optional
+	Topology Topology `json:"topology,omitempty"`
+}
+
+// TopologyMode defines the topology mode for the secondary scheduler instances.
+// +kubebuilder:validation:Enum="";SingleReplica;HighlyAvailable
+type TopologyMode string
+
+const (
+	// "HighlyAvailable" is for secondary scheduler instances to configure high-availability as much as possible.
+	HighlyAvailableMode TopologyMode = "HighlyAvailable"
+
+	// "SingleReplica" is for a single secondary scheduler instances to avoid spending resources for high-availability purpose.
+	SingleReplicaMode TopologyMode = "SingleReplica"
+)
+
+// Topology allows to configure the secondary schedulers to run in an HA mode
+// +kubebuilder:validation:XValidation:rule="self.mode == 'HighlyAvailable' || !has(self.highlyAvailableTopology)",message="highlyAvailableTopology can only be set when mode is HighlyAvailable"
+type Topology struct {
+	// mode defines the topology mode for the secondary scheduler instances.
+	// If unspecified, mode defaults to SingleReplica. The default is subject to change over time.
+	// +kubebuilder:default=SingleReplica
+	// +unionDiscriminator
+	Mode TopologyMode `json:"mode,omitempty"`
+
+	// highlyAvailableTopology provides configuration for HA mode
+	// If empty, defaults will be applied. The defaults are subject to change over time.
+	// +optional
+	HighlyAvailableTopology *HighlyAvailableTopology `json:"highlyAvailableTopology,omitempty"`
+}
+
+type HighlyAvailableTopology struct {
+	// If specified, the node selector for the target nodes.
+	// If unspecified all nodes are considered (default when no node selector is provided).
+	// The default is subject to change over time.
+	NodeSelector *map[string]string `json:"nodeSelector,omitempty"`
+
+	// If specified, the pod's tolerations.
+	// If unspecified no taint is tolerated (default when no toleration is provided).
+	// The default is subject to change over time.
+	Tolerations *[]corev1.Toleration `json:"tolerations,omitempty"`
+
+	// maxReplicas defines the maximum number of replicas for the secondary scheduler.
+	// In HA mode, the actual number of replicas is determined by the number of nodes
+	// matching the nodeSelector, but will not exceed maxReplicas if specified.
+	// If unspecified, maxReplicas defaults to 3.
+	// The default is subject to change over time.
+	// +kubebuilder:default=3
+	// +kubebuilder:validation:Minimum=1
+	MaxReplicas uint32 `json:"maxReplicas,omitempty"`
 }
 
 // SecondarySchedulerStatus defines the observed state of SecondaryScheduler
