b67721cfe4e01b252cee106e6fd6b80fba32c563
Jan Chaloupka <jchaloup@redhat.com>
2026-05-01 19:31:46 +0200

    chore(generate-fbc.sh): detect the ocp dirs automatically

--- generate-fbc.sh
+++ generate-fbc.sh
@@ -1,4 +1,11 @@
-for OCP_VERSION in v4.20; do
+#!/bin/bash
+
+for OCP_VERSION in $(find . -maxdepth 1 -type d -name 'v4.*' | sed 's|^\./||' | sort -t. -k1,1V -k2,2nr); do
     echo "OCP_VERSION: ${OCP_VERSION}"
-    opm alpha render-template basic $OCP_VERSION/catalog-template.json --migrate-level bundle-object-to-csv-metadata > $OCP_VERSION/catalog/openshift-secondary-scheduler-operator/catalog.json;
+    VERSION_NUM=$(echo $OCP_VERSION | cut -d. -f2)
+    if [ "$VERSION_NUM" -ge 17 ]; then
+        opm alpha render-template basic $OCP_VERSION/catalog-template.yaml --migrate-level bundle-object-to-csv-metadata > $OCP_VERSION/catalog/openshift-secondary-scheduler-operator/catalog.json
+    else
+        opm alpha render-template basic $OCP_VERSION/catalog-template.yaml > $OCP_VERSION/catalog/openshift-secondary-scheduler-operator/catalog.json
+    fi
 done
